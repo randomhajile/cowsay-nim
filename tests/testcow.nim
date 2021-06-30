da4beb8442092ec07a3e00587e99615ec092c3f4
Mike Hampton <randomhajile@users.noreply.github.com>
2021-06-30 10:04:21 -0400

    Various fixes. (#2)

--- tests/testcow.nim
+++ tests/testcow.nim
@@ -1,5 +1,6 @@
 import cowsaypkg/cowsay
 import osProc
+import strformat
 import strutils
 import unittest
 from sequtils import map
@@ -13,62 +14,71 @@ const
   wrap = 39
 
 
-proc getRealCowsay(fname, message: string): string =
+proc getRealCowsay(fname: string, think: bool, message: string): string =
+  ## Invokes the system's `cow(say|think)` command and returns the output.
+  ## Uses poEvalCommand because I had issues with cowthink on Ubuntu in tests.
+  let cmd = if think: "cowthink" else: "cowsay"
   return map(
     execProcess(
-      "cowsay",
-      args = [
-        "-f", fname,
-        "-e", "oo",
-        message
-    ],
-    options = {poUsePath}
+      &"{cmd} -f {fname} -e oo '{message}'",
+      options = {poUsePath, poEvalCommand}
   ).splitLines(),
     proc(x: string): string = x.strip(leading = false),
   ).join("\n").replace("\t", "        ").strip
 
 
 test "hello world matches real cowsay":
-  for fname in walkFiles("data/*.cow"):
-    let
-      (_, cowName, _) = splitFile(fname)
-      expected = getRealCowsay(cowName & ".cow", message)
-      c = Cow(
-        think: think,
-        message: message,
-        eyes: eyes,
-        tongue: tongue,
-        wrap: wrap,
-        file: $cowName & ".cow"
-      )
-    check c.say.strip == expected
+  for fname in walkFiles("src/cowsaypkg/cows/*.cow"):
+    let (_, cowName, _) = splitFile(fname)
+    for think in [true, false]:
+      let
+        expected = getRealCowsay(cowName & ".cow", think, message)
+        c = Cow(
+          think: think,
+          message: message,
+          eyes: eyes,
+          tongue: tongue,
+          wrap: wrap,
+          file: $cowName & ".cow"
+        )
+      check c.say.strip == expected
 
-test "works correctly on reasonable text":
+test "matches real cowsay on reasonable text":
   const
-    message = "The Donkey-Headed Adversary of Humanity opens the discussion."
-  let
-    expected = getRealCowsay("default.cow", message)
-    c = Cow(
-      think: think,
-      message: message,
-      eyes: eyes,
-      tongue: tongue,
-      wrap: wrap,
-      file: "default.cow"
-    )
-  check c.say.strip == expected
+    messages = @[
+      "The Donkey-Headed Adversary of Humanity opens the discussion.",
+      "Doc, note: I dissent. A fast never prevents a fatness. I diet on cod.",
+    ]
+  for think in [true, false]:
+    for message in messages:
+      let
+        expected = getRealCowsay("default.cow", think, message)
+        c = Cow(
+          think: think,
+          message: message,
+          eyes: eyes,
+          tongue: tongue,
+          wrap: wrap,
+          file: "default.cow"
+        )
+      check c.say.strip == expected
 
-test "works correctly on unreasonable text":
+test "matches real cowsay on unreasonable text":
   const
-    message = "aaaaaaa aaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-  let
-    expected = getRealCowsay("default.cow", message)
-    c = Cow(
-      think: think,
-      message: message,
-      eyes: eyes,
-      tongue: tongue,
-      wrap: wrap,
-      file: "default.cow"
-    )
-  check c.say.strip == expected
+    messages = [
+      "aaaaaaa aaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    ]
+  for think in [true, false]:
+    for message in messages:
+      let
+        expected = getRealCowsay("default.cow", think, message)
+        c = Cow(
+          think: think,
+          message: message,
+          eyes: eyes,
+          tongue: tongue,
+          wrap: wrap,
+          file: "default.cow"
+        )
+      check c.say.strip == expected
